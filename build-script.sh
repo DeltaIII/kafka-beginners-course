48cced88c575325bdcacbc7a01f6fc66f853ae4c
Hamzaan <hamzaan.bridle@bjss.com>
2019-11-21 15:07:21 +0000

    update to script

--- build-script.sh
+++ build-script.sh
@@ -1,2 +1,1 @@
-docker run -it -v maven-repo:/root/.m2 maven:3.6.2-jdk-8 mvn archetype:generate
-$ docker run -it --rm -v "$PWD":/usr/src/mymaven -v maven-repo:/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven  maven:3.6.2-jdk-8 mvn clean install  
+docker run -it --rm -v "$PWD":/usr/src/mymaven -v maven-repo:/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven  maven:3.6.2-jdk-8 mvn clean install  
