c9b7be5b145b6f1a376b033fe7c5895b41436566
Hamzaan <hamzaan.bridle@bjss.com>
2019-11-21 14:41:28 +0000

    Adding the constants project

---
+++ kafka-constants/src/main/java/org/kafka/constants/NetworkConstants.java
@@ -0,0 +1,16 @@
+package org.kafka.constants;
+
+/**
+ * Networking constants for connecting to different kafka hosts in the swarm
+ */
+public final class NetworkConstants {
+
+	public static final String BROKER_HOST_NAME = "kafka_kafka";
+	public static final String BROKER_PORT = "9092";
+	public static final String BOOTSTRAP_SERVER = BROKER_HOST_NAME + ":" + BROKER_PORT;
+
+	public static final String ZOOKEEPER_HOST_NAME = "kafka_zookeeper";
+	public static final String ZOOKEEPER_PORT = "2181";
+	public static final String ZOOKEEPER_SERVER = ZOOKEEPER_HOST_NAME + ":" + ZOOKEEPER_PORT;
+
+}
---
+++ kafka-constants/src/main/java/org/kafka/constants/Topics.java
@@ -0,0 +1,10 @@
+package org.kafka.constants;
+
+/**
+ * Kafka Topics used
+ */
+public class Topics {
+
+	public static final String FIRST = "first_topic2";
+
+}
