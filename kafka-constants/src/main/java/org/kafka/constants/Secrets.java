d746e197ceec98820c32b1df715d9a18898412c9
Hamzaan <hamzaan.bridle@bjss.com>
2019-11-22 12:59:42 +0000

    Adding missing secrets class

---
+++ kafka-constants/src/main/java/org/kafka/constants/Secrets.java
@@ -0,0 +1,11 @@
+package org.kafka.constants;
+
+public class Secrets {
+
+	// TODO Move secrets to something more secure!!!!
+	public static final String API_KEY = "mPZ2HvwcrSFsS7JgqQKrBRQa4";
+	public static final String API_SECRET = "TKsyFzwS7poHnZBG4jMDTuRrqRV5BZa38pbi9ZKfwFYSqaDgQ3";
+	public static final String ACCESS_TOKEN = "1197568719565799424-Lgw3Si4HIBfQpHYsU7UCX0cXCIvCaC";
+	public static final String ACCESS_SECRET = "kGc5bAflxv4PE9ixH394TYBPUSOuDeXaflD2EmWQmDwmA";
+
+}
